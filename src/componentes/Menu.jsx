b46ededfc845c1ba3319fdf88fc92c0c7ec7d262
Bastian <bastij58@gmail.com>
2024-12-28 21:11:27 -0300

    subiendo logica

--- src/componentes/Menu.jsx
+++ src/componentes/Menu.jsx
@@ -1,21 +1,26 @@
 import { useState } from "react"
 
 
+const getMenu = async ()=>{
+  const resp = await fetch('./bd/Menu.json');
+  const data = await resp.json();
 
+  return data
+}
 
 export const Menu = () => {
 
 const [menu, setmenu] = useState([])
 
-const getMenu = async ()=>{
-  const resp = await fetch('./bd/Menu.json');
-  const data = await resp.json();
-  console.log(data)
+useEffect(() => {
+  const datos = getMenu();
+  setmenu(datos);
   
-}
+}, [])
 
 
-getMenu()
+
+
 
 
 
