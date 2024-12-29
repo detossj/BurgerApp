2f6135040af519f7839c8d4dc897d01c051f6fd3
Bastian <bastij58@gmail.com>
2024-12-28 21:30:30 -0300

    agregue logica al menu y muestra los datos

--- src/componentes/MenuItem.jsx
+++ src/componentes/MenuItem.jsx
@@ -1,14 +1,14 @@
 
 
-export const MenuItem = ({burguer}) => {
+export const MenuItem = ({nombre,precio}) => {
 
     
 
   return (
     <div>
         <img src="" alt="Imagen de Hamburguesa referencia" />   
-        <p>Nombre</p>
-        <p>precio</p>
+        <p>{nombre}</p>
+        <p>{precio}</p>
     </div>
   )
 }
