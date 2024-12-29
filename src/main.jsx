683803e1f18c71ae1419a74be3531b8cc55c428f
detossj <jorgerubilarssj@gmail.com>
2024-12-29 13:15:27 -0300

    Arreglo css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './componentes/App'
 import './estilos/Header.css'
+import './estilos/button.css'
+import './estilos/index.css'
+import './estilos/menuItem.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App></App>
