767b35dcd2c9914af6224f237355f500efe5119d
detossj <jorgerubilarssj@gmail.com>
2025-01-01 15:44:01 -0300

    creado build

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -5,14 +5,14 @@ export const Header = () => {
     <header >
         
         <div className="slogan">
-        <img src="./public/images/602eded1674b3f43321f3874da4804c9.png"  alt="" />
+        <img src="images/602eded1674b3f43321f3874da4804c9.png"  alt="" />
           <div className="text">
             <h1>Hamburguesas Premier</h1>
             <span>Las Hamburguesas más ricas de Concepción</span>
           </div>
           <div className="socials">
-            <a href="#"><img src="./public/images/facebook.png" alt="Facebook" /></a>
-            <a href="#"><img src="./public/images/instagram.png" alt="Instagram" /></a>
+            <a href="#"><img src="/images/facebook.png" alt="Facebook" /></a>
+            <a href="#"><img src="/images/instagram.png" alt="Instagram" /></a>
           </div>
         </div>
 
